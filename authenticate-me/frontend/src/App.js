6bf40e3acb53c5510b03abcc123003b9eb90cb2f
Sunny Mallick <sunny.mallick927@gmail.com>
2021-07-16 15:24:58 -0600

    login page constructed

--- authenticate-me/frontend/src/App.js
+++ authenticate-me/frontend/src/App.js
@@ -1,17 +1,9 @@
-import React, { useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { Route, Switch } from "react-router-dom";
-import LoginFormPage from "./components/LoginFormPage";
-import * as sessionActions from "./store/session";
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import LoginFormPage from './components/LoginFormPage';
 
 function App() {
-  const dispatch = useDispatch();
-  const [isLoaded, setIsLoaded] = useState(false);
-  useEffect(() => {
-    dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
-  }, [dispatch]);
-
-  return isLoaded && (
+  return (
     <Switch>
       <Route path="/login">
         <LoginFormPage />

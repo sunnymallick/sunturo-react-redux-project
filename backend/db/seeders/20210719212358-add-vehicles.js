9d595ef4151adcc0c15ac4c9f272cd5c5a2b21d4
Sunny Mallick <sunny.mallick927@gmail.com>
2021-07-19 16:28:48 -0600

    finished basic seed data

--- backend/db/seeders/20210719212358-add-vehicles.js
+++ backend/db/seeders/20210719212358-add-vehicles.js
@@ -65,6 +65,34 @@ module.exports = {
       createdAt: new Date(),
       updatedAt: new Date()
      },
+     {
+      userId: 2,
+      year: '2006',
+      make: 'Lamborghini',
+      model: 'Gallardo',
+      type: 'Luxury',
+      ageRestriction: true,
+      price: 299.00,
+      lat: 40.7608,
+      lng: 111.8910,
+      description: `Here is my Lamborghini Gallardo that you can book! 25+ only. If you've ever wanted to live out your dream of jumping up in your Lamborghini Gallardo, you can! Just know that any damages to the leather or upholstory is subject to a $500 fine.`,
+      createdAt: new Date(),
+      updatedAt: new Date()
+     },
+     {
+      userId: 2,
+      year: '2017',
+      make: 'Ford',
+      model: 'Focus',
+      type: 'Hatchback',
+      ageRestriction: false,
+      price: 150.00,
+      lat: 40.7608,
+      lng: 111.8910,
+      description: 'Here is my Ford Focus ST that you can book! Even comes with a crackle tune so you can give your neighbors another reason to hate you! Respect the high booking price, I know what I have.',
+      createdAt: new Date(),
+      updatedAt: new Date()
+     },
   ], {});
   },
 

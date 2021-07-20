0b0c4e3c4853c73a77124c8f36197c87d4edc655
Sunny Mallick <sunny.mallick927@gmail.com>
2021-07-19 23:27:04 -0600

    adding image for homepage

--- frontend/src/components/HomePage/HomePage.js
+++ frontend/src/components/HomePage/HomePage.js
@@ -2,7 +2,10 @@ import "./HomePage.css"
 
 const HomePage = () => {
     return (
-        <p>Welcome to the HomePage</p>
+        // <p>Welcome to the HomePage</p>
+        <div>
+        <img src='../../images/shelby-homepage.jpg' alt='shelbypic' />
+        </div>
     )
 }
 

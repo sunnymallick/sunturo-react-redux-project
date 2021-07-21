450dda5d30838efbe2a52a7b913bc80af96d7fed
Sunny Mallick <sunny.mallick927@gmail.com>
2021-07-21 10:19:41 -0600

    adding features to booking page

--- frontend/src/components/VehicleBooking/VehicleBooking.js
+++ frontend/src/components/VehicleBooking/VehicleBooking.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { getOneVehicle } from '../../store/vehicle';
@@ -8,7 +8,6 @@ const VehicleBooking = () => {
     const { id } = useParams();
     // console.log(id)
     const vehicle = useSelector((state) => state.vehicles[id])
-    console.log(vehicle)
 
     useEffect(() => {
         dispatch(getOneVehicle(id))
@@ -17,19 +16,36 @@ const VehicleBooking = () => {
     return (
         <>
        <div>
-        <p>The car: {vehicle.year} {vehicle.make} {vehicle.model}</p>
+        <p>The car: {vehicle?.year} {vehicle?.make} {vehicle?.model}</p>
 
-        <p>Hosted by</p>
+        <p>Hosted by </p>
 
-        <p>Price: ${vehicle.price} per day</p>
+        <p>Price: ${vehicle?.price} per day</p>
 
-        <p>{vehicle.description}</p>
+        <p>{vehicle?.description}</p>
        </div>
 
         <div className='booking'>
             <form>
+                <label>Trip Start: </label>
                 <input
-                    type='date' />
+                    type='date'
+                    placeholder='From'
+                    required/>
+                <input
+                    type='time'
+                    required />
+                    <br></br>
+                <label>Trip End:</label>
+                <input
+                    type='date'
+                    placeholder='To'
+                    required/>
+                <input
+                    type='time'
+                    required />
+                    <br></br>
+                <button type='submit'>Continue</button>
             </form>
         </div>
     </>

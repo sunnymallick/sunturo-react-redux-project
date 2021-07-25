1d1511170769a5124877a1a04d405bb115ba162e
Sunny Mallick <sunny.mallick927@gmail.com>
2021-07-25 01:17:27 -0600

    booking delete is functional

--- frontend/src/components/BookingsPage/UserBookings.js
+++ frontend/src/components/BookingsPage/UserBookings.js
@@ -1,26 +1,52 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams, Link } from 'react-router-dom';
-import { getBookings } from '../../store/booking';
+import { getBookings, deleteBooking } from '../../store/booking';
 
 const UserBookings = () => {
     const dispatch = useDispatch();
     const { id } = useParams();
 
     const bookings = useSelector((state) => ((Object.values(state.bookings))));
-    console.log(bookings)
     const listingBookings = bookings.filter(booking => booking.userId === +id)
     console.log(listingBookings)
-    const sessionUser = useSelector(state => state.session.user);
 
     useEffect(() => {
         dispatch(getBookings())
-    }, [dispatch])
+        dispatch(deleteBooking(id))
+    }, [dispatch, id])
+
+    const handleDelete = (id) => {
+        dispatch(deleteBooking(id))
+    }
+
+    let content;
+    if (!listingBookings.length) {
+        content = (
+            <p>You currently do not have any bookings.</p>
+        )
+    } else {
+        content = (
+            <div className='userBookings'>
+            <h5>Bookings</h5>
+                {listingBookings.map((booking) => {
+                    return (
+                        <>
+                            <div>
+                                <p>{booking?.Vehicle?.year} {booking?.Vehicle?.make} {booking?.Vehicle?.model}</p>
+                                <p>Start Date: {new Date (booking.startDate).toLocaleDateString()} End Date: {new Date (booking.endDate).toLocaleDateString()}</p>
+                                <button type='button' onClick={() => handleDelete(booking.id)}>Delete Booking</button>
+                            </div>
+                        </>
+                    )
+                })}
+        </div>
+        )
+    }
 
     return (
         <>
-            <p>This is the user bookings page</p>
-            <div className='userBookings'>
+            {/* <div className='userBookings'>
                 <h5>Bookings</h5>
                     {listingBookings.map((booking) => {
                         return (
@@ -32,7 +58,8 @@ const UserBookings = () => {
                             </>
                         )
                     })}
-            </div>
+            </div> */}
+            {content}
         </>
     )
 
